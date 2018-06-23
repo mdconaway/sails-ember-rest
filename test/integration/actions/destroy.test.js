1199901b87f5b121bcf4ce33bc663fe5a06c8f83
Adam Gribble <adam@thehumangeo.com>
2018-06-23 15:21:50 +0100

    Updated tests for the destroy action

--- test/integration/actions/destroy.test.js
+++ test/integration/actions/destroy.test.js
@@ -1,33 +1,43 @@
 import supertest from 'supertest';
-const newFoo = {
-  foo: {
-    name: 'destroyFoo',
-    myBar: 1
+const newArticle = {
+  data: {
+    type: 'articles',
+    attributes: {
+      title: 'I Woke Up in a Car'
+    },
+    relationships: {
+      author: {
+        data: {
+          type: 'authors',
+          id: '2'
+        }
+      }
+    }
   }
 };
-let targetFoo = null;
+let targetArticle = null;
 
 describe('Integration | Action | destroy', function() {
   beforeEach(function(done) {
-    Foo.create(newFoo.foo).exec((err, record) => {
+    Articles.create(Object.assign({}, newArticle.attributes, { author: newArticle.data.relationships.author.data.id })).exec((err, record) => {
       if (err) {
         return done(err);
       }
-      targetFoo = record;
+      targetArticle = record;
       done();
     });
   });
 
   describe(':: response format', function() {
-    it('should respond with status code 200', function(done) {
+    it('should respond with status code 204', function(done) {
       supertest(sails.hooks.http.app)
-        .delete(`/foos/${targetFoo.id}`)
-        .expect(200)
+        .delete(`/articles/${targetArticle.id}`)
+        .expect(204)
         .end(done);
     });
     it('should return a null response text', function(done) {
       supertest(sails.hooks.http.app)
-        .delete(`/foos/${targetFoo.id}`)
+        .delete(`/articles/${targetArticle.id}`)
         .expect(res => {
           expect(res.text).to.equal('null');
         })
@@ -36,9 +46,9 @@ describe('Integration | Action | destroy', function() {
   });
 
   describe(':: data integrity', function() {
-    it('should return no foos', function(done) {
+    it('should return no articles', function(done) {
       supertest(sails.hooks.http.app)
-        .delete(`/foos/${targetFoo.id}`)
+        .delete(`/articles/${targetArticle.id}`)
         .expect(res => {
           expect(res.body).to.equal(null);
         })
@@ -46,13 +56,13 @@ describe('Integration | Action | destroy', function() {
     });
     it('should remove target record from the database', function(done) {
       supertest(sails.hooks.http.app)
-        .delete(`/foos/${targetFoo.id}`)
+        .delete(`/articles/${targetArticle.id}`)
         .expect(res => {
           expect(res.body).to.equal(null);
         })
         .end(() => {
           supertest(sails.hooks.http.app)
-            .get(`/foos/${targetFoo.id}`)
+            .get(`/articles/${targetArticle.id}`)
             .expect(404)
             .end(done);
         });
