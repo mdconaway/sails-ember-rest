c681ec512e5234689e5aa10ce352c6bd202ccad9
Adam Gribble <adam@thehumangeo.com>
2018-06-30 23:01:25 +0100

    Implemented JSONAPISerializer as a global via hook

--- index.js
+++ index.js
@@ -185,6 +185,9 @@ module.exports.Actions = module.exports.actions = {
   Update: require('./templates/actions/update')
 };
 module.exports.Controller = module.exports.controller = require('./templates/controllers/EmberController');
+module.exports.Hooks = module.exports.hooks = {
+  registerSerializers: require('./templates/hooks/register-serializers')
+};
 module.exports.Service = module.exports.service = require('./templates/services/Ember');
 module.exports.Policies = module.exports.policies = {
   emberCreate: require('./templates/policies/emberCreate'),
---
+++ templates/hooks/register-serializers/index.js
@@ -0,0 +1,68 @@
+/**
+ * register-serializers hook
+ *
+ * @description :: Extend the sails object with a global serializer with all registered models
+ * @docs        :: https://sailsjs.com/docs/concepts/extending-sails/hooks
+ */
+
+const { kebabCase } = require('lodash');
+const pluralize = require('pluralize');
+const Serializer = require('json-api-serializer');
+
+// Link the JSONAPISerializer to the global namespace
+global.JSONAPISerializer = new Serializer({
+  convertCase: 'kebab-case',
+  unconvertCase: 'camelCase'
+});
+
+module.exports = function defineRegisterSerializersHook(sails) {
+
+  return {
+
+    /**
+     * Runs when a Sails app loads/lifts.
+     *
+     * @param {Function} done
+     */
+    initialize: function (done) {
+
+      sails.log.info('Initializing custom hook (`register-serializers`)');
+
+      sails.on('hook:orm:loaded', function() {
+        Object.keys(sails.models).forEach((modelName) => {
+          const Model = sails.models[modelName];
+          const type = pluralize(kebabCase(Model.globalId));
+          const relationships = Model.associations
+            .reduce((acc, { alias, type }) => {
+              return Object.assign({}, acc, {
+                [alias]: {
+                  type: kebabCase(type === 'model' ? pluralize(alias) : alias),
+                  links(data) {
+                    const base = Ember.generateResourceLink(type, data.id);
+                    return {
+                      related: `${base}/${alias}`,
+                      self: `${base}/relationships/${alias}`
+                    }
+                  }
+                }
+              });
+            }, {});
+
+          JSONAPISerializer.register(type, {
+            links: {
+              self(data) {
+                return Ember.generateResourceLink(type, data.id);
+              }
+            },
+            relationships
+          });
+        });
+
+      });
+
+      return done();
+    }
+
+  };
+
+};
